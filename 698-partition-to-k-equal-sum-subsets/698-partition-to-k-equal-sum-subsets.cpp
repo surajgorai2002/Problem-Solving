5005dd55f0f408a7a76c86c38bf91da3152bf973
Suraj Gorai <89802077+surajgorai2002@users.noreply.github.com>
2022-07-28 11:27:50 +0530

    Time: 2391 ms (5.00%), Space: 9.3 MB (96.35%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int vis[17];
+    bool check(vector<int>&nums,int bn,  int k , int currsum,int reqsum,int idx)
+    {
+        if(bn==k)
+        {
+            return true ;
+        }
+        if(currsum==reqsum)
+        {
+            return check(nums,bn+1,k,0,reqsum,0);
+        }
+        if(currsum>reqsum)
+        {
+            return false ;
+        }
+        if(idx>=nums.size())
+        {
+            
+            return false;
+        }
+        if(!vis[idx])
+        {
+            vis[idx]=1;
+           bool k1= check(nums,bn,k,currsum+nums[idx],reqsum,idx+1);
+            if(k1)
+                return true ;
+            vis[idx]=0;
+           bool k2=check(nums,bn,k,currsum,reqsum,idx+1);
+            return k1|k2;
+        }
+        else
+        {
+           return check(nums,bn,k,currsum,reqsum,idx+1);
+        }
+        return false;
+    }
+    
+    
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+      int s=0;
+        int maxi=INT_MIN;
+        for(int x:nums)
+        {
+            s+=x;
+            maxi=max(maxi,x);
+        }
+        if(s%k!=0)
+        {
+            return false;
+        }
+        int reqsum=s/k;
+        if(maxi>reqsum)
+        {
+            return false;
+        }
+       memset(vis,0,sizeof(vis));
+        return check(nums,0,k,0,reqsum,0);
+    }
+};
